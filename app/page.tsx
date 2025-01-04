387b0a8f6a4ede47279621cb9d436ea34e765960
Amaljith <amaljith6430@gmail.com>
2025-01-04 21:42:54 +0530

    Update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,6 @@ export default function Home() {
       <header className="relative w-full -mb-52 flex items-center justify-center overflow-hidden bg-[radial-gradient(50%_50%_at_50%_50%,#4a0979_0%,#5f0000_50%,rgba(139,0,255,0)_100%)]">
         <Hero />
       </header>
-
-      {/* Main Content */}
-      <main className="relative w-full h-full bg-black">
-        <BackgroundCellAnimation/>
-      </main>
     </>
   );
 }
