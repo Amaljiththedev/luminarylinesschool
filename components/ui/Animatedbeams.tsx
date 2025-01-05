6fcccef450192d1bc79dae27ae3177e5d70b0695
Amaljith <amaljith6430@gmail.com>
2025-01-05 12:16:16 +0530

    build version2223

--- components/ui/Animatedbeams.tsx
+++ components/ui/Animatedbeams.tsx
@@ -11,7 +11,7 @@ const Hero = () => (
       <div
         style={{
           background:
-            "radial-gradient(circle at center, #005f73 0%, #0a9396 20%, rgba(0, 173, 181, 0) 60%)",
+            "radial-gradient(circle at center, #016652 0%, #033d3c 20%, rgba(1, 102, 82, 0) 60%)",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
@@ -22,31 +22,31 @@ const Hero = () => (
         {/* Hero Content */}
         <p className="text-lg md:text-xl font-poppins mb-4 text-gray-300">
           Take your skills{" "}
-          <span className="bg-gradient-to-r from-teal-200 via-teal-400 to-teal-600 bg-clip-text font-extrabold text-transparent">
+          <span className="bg-gradient-to-r from-white via-red-500 to-cyan-500 bg-clip-text font-extrabold text-transparent">
             From Beginner to Pro
           </span>{" "}
           in just 3 months
         </p>
 
-        <h1 className="md:text-2xl lg:text-7xl font-medium text-center bg-clip-text text-transparent bg-gradient-to-b from-teal-300 to-teal-500 pointer-events-none">
+        <h1 className="md:text-2xl lg:text-7xl font-medium text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-400 pointer-events-none">
           Master Video Editing
           <br className="hidden md:inline" />
           The Ultimate Career-Oriented Cohort
         </h1>
         <p className="text-md md:text-lg font-sans mb-8 text-gray-200">
           Over{" "}
-          <span className="bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 bg-clip-text text-transparent font-semibold">
+          <span className="bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600 bg-clip-text text-transparent font-semibold">
             3000+
           </span>{" "}
           Students Trained,{" "}
-          <strong className="bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 bg-clip-text text-transparent">
+          <strong className="bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600 bg-clip-text text-transparent">
             85% Average Placement Rate*
           </strong>
         </p>
 
         {/* Waitlist Button */}
         <div className="group relative">
-          <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 opacity-75 blur transition duration-500 group-hover:opacity-100"></div>
+          <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-75 blur transition duration-500 group-hover:opacity-100"></div>
           <button className="relative rounded-lg bg-black px-7 py-4 text-white">
             Join The Waitlist
           </button>
